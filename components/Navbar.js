eb89e6ca065c04106545e160d6e85305c740bf29
Izzeldin Addarda <zeldin26@gmail.com>
2022-04-10 18:19:32 +0700

    Merge branch 'master' of https://github.com/ridhoadtyaa/CoChat

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,11 +1,32 @@
 import { useState } from 'react';
+let navItemsState = {
+  links: [
+    {
+      id: 1,
+      name: 'Beranda',
+      href: '#',
+    },
+    {
+      id: 2,
+      name: 'Fitur-fitur',
+      href: '#fitur',
+    },
+    {
+      id: 3,
+      name: 'Teknologi',
+      href: '#teknologi',
+    },
+    {
+      id: 4,
+      name: 'FAQ',
+      href: '#faq',
+    },
+  ],
+};
 
-const NavItem = ({ children, href, onClick }) => {
+const NavItem = ({ item, children, href, onClick, className }) => {
   return (
-    <li
-      className={`text-lg text-white transition duration-300 hover:text-blue-200 md:text-black md:hover:text-blue-500`}
-      onClick={onClick}
-    >
+    <li className={className} onClick={onClick}>
       <a href={href}>{children}</a>
     </li>
   );
@@ -13,7 +34,7 @@ const NavItem = ({ children, href, onClick }) => {
 
 const Navbar = () => {
   const [offCanvas, setOffCanvas] = useState(false);
-
+  const [activeId, setActiveId] = useState(1);
   return (
     <nav>
       <div className="container mx-auto flex items-center justify-between py-8">
@@ -29,7 +50,7 @@ const Navbar = () => {
         <div
           className={`fixed top-0 ${
             offCanvas ? 'left-0' : '-left-full'
-          } n md:w-auto z-50 h-full w-full bg-gradient-to-r from-blue-500 to-blue-600 transition-all  duration-300 md:static md:bg-none`}
+          } n z-50 h-full w-full bg-gradient-to-r from-blue-500 to-blue-600 transition-all duration-300  md:static md:w-auto md:bg-none`}
         >
           <div className="flex flex-row-reverse items-start justify-between px-4 pt-8 md:px-0 md:pt-0">
             <button className="md:hidden" onClick={() => setOffCanvas(false)}>
@@ -40,10 +61,43 @@ const Navbar = () => {
             </button>
 
             <ul className="flex flex-col space-y-6 md:mt-0 md:flex-row md:space-y-0 md:space-x-10">
-              <NavItem href="#" onClick={() => setOffCanvas(false)}>
+              {navItemsState.links.map((item) => {
+                return (
+                  <NavItem
+                    item={item}
+                    key={item.id}
+                    href={item.href}
+                    onClick={() => {
+                      setActiveId(item.id);
+                      setOffCanvas(false);
+                    }}
+                    className={`text-lg text-white transition duration-300 hover:text-blue-200 md:text-black md:hover:text-blue-500 ${
+                      activeId === item.id ? 'font-semibold' : ''
+                    }`}
+                  >
+                    {item.name}
+                  </NavItem>
+                );
+              })}
+              {/* <NavItem
+                href="#"
+                onClick={() => {
+                  setOffCanvas(false);
+                  setActiveNav(true);
+                }}
+                className={`${activeNav === true ? 'font-semibold' : ''}`}
+              >
                 Beranda
               </NavItem>
-              <NavItem href="#fitur" onClick={() => setOffCanvas(false)}>
+              <NavItem
+                href="#fitur"
+                className={`${activeNav === 'fitur' ? 'font-semibold' : ''}`}
+                onClick={() => {
+                  setOffCanvas(false);
+                  setActiveNav('fitur');
+                  console.group(activeNav);
+                }}
+              >
                 Fitur-fitur
               </NavItem>
               <NavItem href="#teknologi" onClick={() => setOffCanvas(false)}>
@@ -51,7 +105,7 @@ const Navbar = () => {
               </NavItem>
               <NavItem href="#faq" onClick={() => setOffCanvas(false)}>
                 FAQ
-              </NavItem>
+              </NavItem> */}
             </ul>
           </div>
         </div>
