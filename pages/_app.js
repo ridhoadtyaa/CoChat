a454714de6524b88e4fbd2836d701bc970eef89e
ridhoadtyaa <r2603an@gmail.com>
2022-04-25 15:20:08 +0700

    feat: add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,32 @@
-import "../styles/globals.css";
+import Script from 'next/script';
+import '../styles/globals.css';
+
+// <!-- Global site tag (gtag.js) - Google Analytics -->
+// <script async src="https://www.googletagmanager.com/gtag/js?id=G-ZTLJCEKXY9"></script>
+// <script>
+
+// </script>
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+
+      <Script strategy="lazyOnload" id="google-analytics">
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', ${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS});
+        `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
